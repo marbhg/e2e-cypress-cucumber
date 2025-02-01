0e33cbf656826672c0da090b85c81871d900d956
Marrbe <132088541+marbhg@users.noreply.github.com>
2025-02-01 13:39:37 +0100

    act
    atct

--- cypress/e2e/step_definitions/headre-steps.js
+++ cypress/e2e/step_definitions/headre-steps.js
@@ -5,5 +5,5 @@ import {
 } from "@badeball/cypress-cucumber-preprocessor";
 
 Then ("the user see title Developed with HTML5, CSS3 and Javascrip on header", () => {
-
+  cy.get('data-testid="devename"').should('have.text', 'Developed with HTML5, CSS3 and Javascrip');
 }); 

fc578c8b92d3e3ef422f33552b610eec51f5fba1
Marrbe <132088541+marbhg@users.noreply.github.com>
2025-02-08 19:55:22 +0100

    act
    cypress

--- cypress/e2e/step_definitions/question.js
+++ cypress/e2e/step_definitions/question.js
@@ -4,21 +4,29 @@ import {
   Then,
 } from "@badeball/cypress-cucumber-preprocessor";
 
-When("the user select option {string} on question number {string}", (option, resp) => {
-  //Selecciona la pregunta correcta + incorrecta + vacia 
-  cy.get('[data-testid="resp1" ' + resp + '"]').select(option);
-});
+When("the user selects option {string} on question number {string}", (option, questionNumber) => {
+  cy.get('[data-testid="resp${questionNumber}"]').select(option);
+  cy.get('[data-testid="resp${questionNumber}"]').should('have.value', option);
 
-When("the user click on the Evaluate everything button", () => {
+  cy.get('[data-testid="resp${questionNumber}"] option:selected').should('have.text', getOptionText(option));
+
+  cy.get('[data-testid="resultadopregunta${questionNumber}"]').should('be.visible');
+  // ... aserciones específicas para el resultado de la pregunta
+});
+function getOptionText(optionValue) {
+  // ... (misma función que antes)
+}
+
+Then("the user click on the Evaluate everything button", () => {
   //Validamos el boton para ingresar al examen  
   cy.get('[data-testid="buttonfinish"]').click();
 });
 
-When("the user should see score {string} for question {string}", (option, resp) => {
+//*When("the user should see score {string} for question {string}", (option, resp1) => {
   //Validar la puntuacion de las preguntas 
-  cy.get('[data-testid="resultadopregunta1"]').should('contain', option);
-});
+  //*cy.get('[data-testid="resultadopregunta1" ' + resp1 + '"]').should('contains', option);
+//*});
 
-//*When("The user must see the score in green if they are approved ", () => {
+//*Then("The user must see the score in green if they are approved ", () => {
 //El usuario debe ver la puntuacion en verde si el examen esta aprobado 
 //*});

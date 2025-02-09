edc1440d6f3619bc431c656b256fe8eb1e5cde7b
Marrbe <132088541+marbhg@users.noreply.github.com>
2025-02-09 19:25:22 +0100

    act
    act

--- cypress/e2e/step_definitions/question.js
+++ cypress/e2e/step_definitions/question.js
@@ -4,18 +4,9 @@ import {
   Then,
 } from "@badeball/cypress-cucumber-preprocessor";
 
-When("the user selects option {string} on question number {string}", (option, questionNumber) => {
-  cy.get('[data-testid="resp${questionNumber}"]').select(option);
-  cy.get('[data-testid="resp${questionNumber}"]').should('have.value', option);
-
-  cy.get('[data-testid="resp${questionNumber}"] option:selected').should('have.text', getOptionText(option));
-
-  cy.get('[data-testid="resultadopregunta${questionNumber}"]').should('be.visible');
-  // ... aserciones específicas para el resultado de la pregunta
+When("the user selects option {string} on question number {string}", (option, resp1) => {
+  cy.get('[data-testid="resp1"]').select(option);
 });
-function getOptionText(optionValue) {
-  // ... (misma función que antes)
-}
 
 Then("the user click on the Evaluate everything button", () => {
   //Validamos el boton para ingresar al examen  

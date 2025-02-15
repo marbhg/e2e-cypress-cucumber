b1698e9e06281f238955d295bef4a55c2813c6e7
Marrbe <132088541+marbhg@users.noreply.github.com>
2025-02-15 15:08:52 +0100

    act
    act

--- cypress/e2e/step_definitions/question_steps.js
+++ cypress/e2e/step_definitions/question_steps.js
@@ -16,10 +16,9 @@ Then("the user click on the Evaluate everything button", () => {
 When("the user should see score {string} for question {string}", (option, questionnumber) => {
   //Validar la puntuacion de las preguntas 
   cy.get('[data-testid="resultadopregunta' + questionnumber + '"]').should('exist').should('contain', option);
-  cy.get('#grademessage10').should('exist').should('be.visible').should('contain', '0');
 });
 
-Then("The user must see the score in {string} if they are approved ", (coloresultado) => {
-  //El susuario debe ver el msnaje en verde si esta aprovado y en rojo si esta suspenso 
-  cy.get('[data-testid="finishresults' + coloresultado + '"]').should ( 'have.css' , 'color' , 'rgba(0, 0, 0, 0.87)' )
+Then("the user should see the score in {string} if approved", (coloresultado) => {
+  //El susuario debe ver el msnaje en verde si esta aprobado
+  cy.get('[data-testid="resultado-finales' + coloresultado + '"]').should ( 'have.css' , 'color' , 'rgba(0, 0, 0, 0.87)' )
 });  
